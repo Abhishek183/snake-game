09b376d9c3bdcf8978233fd04bc54f743f015e08
ABHISHEK183 <mishra.mr97@gmail.com>
2020-05-28 16:04:02 +0530

    files commited

---
+++ setuplearn.py
@@ -0,0 +1,6 @@
+from cx_Freeze import setup , Executable
+setup(name = "snakegame",
+      version = "0.1",
+      description ="a snake game ",
+      executables =[Executable(r"C:\Users\abhishek\OneDrive\Documents\snake\learn.py")]
+      )
